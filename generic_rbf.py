aeb6a290a3bdf27b0974da9c2dcbebf1e6bc5cc9
Lalla1093 <lmeneghe@sissa.it>
2019-03-12 15:53:19 +0100

    Add plot method

--- generic_rbf.py
+++ generic_rbf.py
@@ -1,6 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 class GenericRBF(object):
-    def __init__(self):
+    def __call__(self):
         raise NotImplementedError
 
     def plot(self, a=0, b=1):
-        pass
+        x=np.linspace(a,b,1024)
+        plt.figure()
+        plt.plot(x,self(x))
+        plt.show()
